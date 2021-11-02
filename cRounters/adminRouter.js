389c0d13bf53fa0f6bacfa59aa9c44b35e6b5b07
nafis-tech <nafiezlela9@gmail.com>
2021-11-02 15:12:55 +0700

    soal-tugas-siswa

--- cRounters/adminRouter.js
+++ cRounters/adminRouter.js
@@ -3,5 +3,9 @@ const router = require('express').Router()
 const {adminController} = require('../controllers')
 
 router.get('/data-students', adminController.dataStudents)
+router.get('/all-soaltugas', adminController.dataAllSoalTugas)
+router.post('/soal-tugas', adminController.soalTugas)
+router.post('/nilai-tugas', adminController.nilaiTugas)
+router.post('/student-fullname', adminController.dataStudentsById)
 
 module.exports = router
